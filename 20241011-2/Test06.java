42257defef42152def83c029ca840056456d88eb
CHANG-LGTM <dlckdgus145@naver.com>
2024-10-11 17:58:07 +0900

    for문을 이용한 반복 작업 연습

---
+++ 20241011-2/Test06.java
@@ -0,0 +1,12 @@
+public class Test06 {
+    public static void main(String[] args) {
+        int[] a = new int[5];  //배열 선언
+
+
+        for (int i =0; i<a.length; i++)
+            a[i]=5-i;
+
+        for (int i =0; i<a.length; i++)
+            System.out.println("a["+i+"] =" +a[i]);
+    }
+}
