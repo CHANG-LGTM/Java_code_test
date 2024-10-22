51913284ccb819f4a6f7254444fa6cee70dba3bd
CHANG-LGTM <dlckdgus145@naver.com>
2024-10-22 16:23:11 +0900

    문자열을 반환하는 메서드 활용 코드연습

---
+++ 20241022/Test02.java
@@ -0,0 +1,12 @@
+public class Test02 {
+    //---안녕하세요 라고 표시---//
+    static void hello(){
+        System.out.println("안녕하세요.");
+    }
+
+    public static void main(String[] args) {
+        hello();
+        hello();
+        hello();
+    }
+}
